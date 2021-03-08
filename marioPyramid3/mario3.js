6ba95ea6bbc3a2599862ab268289b1e0c6a8441f
dstoutin97 <drewstoutin97@gmail.com>
2021-03-08 01:55:17 +0000

    allow user to input pyramid height and respond to button click

---
+++ marioPyramid3/mario3.js
@@ -0,0 +1,62 @@
+function determineHeightAndThenDrawPyramid() {
+  
+  //Sets height == to input value typed by user
+  let height = document.getElementById("height").value;
+
+  //Creates btn that 'onclick' calls the printPyramid function
+  let btn = document.getElementById("MyBtn").addEventListener("click", printPyramid(height));
+}
+
+
+
+//Building the pyramid
+function printPyramid(height) {
+
+// ==> Find and empty pyramid before starting
+//Grabs pyramid ID from HTML
+let element = document.getElementById("pyramid");
+// ==> Empty `element` contents
+element.innerHTML = '';
+
+let numBricks = 0;
+let numSpaces = 0;
+
+  for (let row = 0; row < height; row++) {
+    let layer = "";
+
+  //figure out number of bricks and spaces
+    numBricks = row + 2;
+    numSpaces = height - row - 1;
+
+    for (let i = 0; i < numSpaces; i++) {
+        layer += ".";
+    }
+    for (let i = 0; i < numBricks; i++) {
+        layer += "#";
+    } 
+      
+      //Prints layer to DOM
+
+      //Creates new paragraph element
+      let para = document.createElement("p");
+      //Creates a text node from layer variable
+      let rowStr = document.createTextNode(layer);
+      //Para becomes parent of rowStr
+      para.appendChild(rowStr);
+      //Element becomes parent of para
+      element.appendChild(para);
+
+      //Summation: 
+      //element/para/rowStr. 
+      //Element == ID of pyramid. 
+      //Para(New paragraph tag) == rowStr.
+      //rowStr == layer variable == string to build pyramid
+
+    
+
+      }
+    
+      
+    }
+    
+
